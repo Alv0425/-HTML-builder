e987ad55f3c8d0da00a61ca9025174ff49283604
Alv0425 <vilejshikova@gmail.com>
2024-01-17 21:37:10 +0300

    feat: finish 02-write-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 const newPath = path.join(__dirname, 'text.txt');
-console.log(newPath);
 
 const newReadStream = fs.createReadStream(newPath, 'utf-8');
 newReadStream.on('data', (data) => {
-  console.log(data);
+  process.stdout.write(data);
+});
+
+newReadStream.on('error', (err) => {
+  process.stdout.write(err);
 });
---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const rl = require('readline');
+const { stdin, stdout } = process;
+
+const fileName = 'newfile.txt';
+const newPath = path.join(__dirname, fileName);
+const message = 'File ' + fileName + ' was successfully updated!';
+
+const newWriteStream = fs.createWriteStream(newPath, {
+  encoding: 'utf8',
+  flags: 'a',
+});
+const newReadLine = rl.createInterface(stdin, stdout);
+const endWrite = () => {
+  newReadLine.write(message);
+  process.exit();
+};
+
+stdout.write('Please, write some test (to exit type "exit"):\n');
+
+newReadLine.on('line', (data) => {
+  if (data === 'exit') {
+    endWrite();
+  } else {
+    newWriteStream.write(data + '\n');
+  }
+});
+
+newReadLine.on('SIGINT', () => {
+  endWrite();
+});

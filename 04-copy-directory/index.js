63e973dca6438d133017c4e777edef28b8a6f0ab
Alv0425 <vilejshikova@gmail.com>
2024-01-21 22:36:45 +0300

    feat: optimize and refactor solution of 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,14 @@
 const path = require('path');
 const { readdir, copyFile, rm, mkdir } = require('fs/promises');
 
-const dirPath = path.join(__dirname, 'files');
+const sourceDirPath = path.join(__dirname, 'files');
 const copyDirPath = path.join(__dirname, 'files-copy');
-const outputPaths = [];
-const dirThree = [];
 
 (async () => {
   await createDir(copyDirPath);
-  await extractInfo(dirPath, copyDirPath);
-  await createDirThree();
-  await copyAll(outputPaths);
+  await extractInfo(sourceDirPath, copyDirPath);
 })();
-// create directory
+
 async function createDir(newPath) {
   try {
     await rm(newPath, { recursive: true, force: true });
@@ -23,31 +19,21 @@ async function createDir(newPath) {
     }
   }
 }
-// copy all files
-async function copyAll(output) {
-  for (const file of output) await copyFile(file.input, file.output);
-}
-// create dir three
-async function createDirThree() {
-  for (const dirPath of dirThree) await createDir(dirPath);
-}
-// run recursively over directory for copying and extract paths of all subfolders and files
-async function extractInfo(npath, destPathBase) {
-  const allFiles = await readdir(npath, { withFileTypes: true });
+
+async function extractInfo(sourcePath, destPathBase) {
+  const allFiles = await readdir(sourcePath, { withFileTypes: true });
   for (const file of allFiles) {
-    const fileName = file.name;
-    const fileDir = file.path;
-    const filePath = path.join(fileDir, fileName);
+    const filePath = path.join(file.path, file.name);
     if (file.isDirectory()) {
       const newBase = path.join(destPathBase, file.name);
-      dirThree.push(newBase);
+      await createDir(newBase);
       await extractInfo(filePath, newBase);
     } else {
-      const iopath = {
+      const copyPath = {
         input: filePath,
         output: path.join(destPathBase, file.name),
       };
-      outputPaths.push(iopath);
+      await copyFile(copyPath.input, copyPath.output);
     }
   }
 }
